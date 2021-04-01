c9490dae3899c1ba9eb54fa762f8508b2abdc422
Seung Woo Choi <choiseun@alumni.usc.edu>
2021-03-31 22:56:19 -0400

    Procfile

--- code/streamlit.py
+++ code/streamlit.py
@@ -1,64 +1,109 @@
+import pandas as pd
 import pickle
 import streamlit as st
-import numpy as np
-import pandas as pd
+from PIL import Image
+
 
 st.set_page_config(
-page_icon='BOOK',
+# page_icon='NONE',
 initial_sidebar_state='expanded'
 )
 
-st.title('Crypto or Stocks?')
+st.title('Measuring Risk Tolerance: Crypto or Stocks?')
+
+st.write('**Disclaimer:** I am not a financial advisor. I am in no way providing any financial advice to anyone in any shape or form. This app should be used solely for personal amusement.')
 
 st.write('Use the sidebar to select a page to view.')
 
 page = st.sidebar.selectbox(
 'Page',
-('About', 'EDA', 'Make a prediction')
+('About', 'Visualize the data', 'Make a prediction')
 )
 
 if page == 'About':
     st.subheader('About this project')
     st.write('''
-    This is a Streamlit app that hosts my r/wallstreetbets vs. r/SatoshiStreetBets model.
+    This is a Streamlit app that showcases my logistic regression model. The model utilizes natural language processing on two subreddits to predict if a post came from r/wallstreetbets or r/SatoshiStreetBets. The predictions from the model can be used to explore risk tolerance in individuals by identifying words unique to each subreddit. Individuals who identify more closely with r/wallstreetbets may find stocks more suitable, whereas those who identify more closely with r/SatoshiStreetBets may find cryptocurrency more suitable.
 
-    The best model I found was Logistic Regression.
-
-    You can get in touch with me on these websites...
+    You can get in touch with me on these websites:
+    - LinkedIn: https://www.linkedin.com/in/seung-woo-choi/
+    - Portfolio: https://choiseun.github.io/
 
     '''
-    )
-
-elif page == 'EDA':
+    )    
+    
+elif page == 'Visualize the data':
     # header
-    st.subheader('Exploratory Data Analysis')
-    st.write('''The model is trained on subreddit posts.
+    st.subheader('Visualize the data')
+    st.write('''Let's look at some colorful visuals.
 
     Below you can find...
 
     '''
     )
-
+    
+    visual_1 = Image.open('../images/wsb_word_cloud.png')
+    st.image(visual_1, caption='Words Unique to r/wallstreetbets')
+    st.write('''
+    
+    ''')
+             
+    visual_2 = Image.open('../images/ssb_word_cloud.png')
+    st.image(visual_2, caption='Words Unique to r/SatoshiStreetBets')
+    st.write('''
+    
+    ''')
+    
+    visual_3 = Image.open('../images/shared_word_cloud.png')
+    st.image(visual_3, caption='Words Common to Both Subreddits')
+    st.write('''
+    
+    ''')
+    
 elif page == 'Make a prediction':
-    st.subheader('Which subreddit suits you better?')
+    st.subheader('Which type of asset suits you better?')
 
     st.write('''
-    Enter some text to make a prediction!
+    Enter some text to make a prediction! The model is trained on subreddit posts.
 
-    The text might visually cut off, but you can write up to 1,000 characters.
+    The text might visually cut off, but you can write up to 500 characters.
     '''
     )
+    
+    st.write('''
+             
+             
+             ''')
+    
+    # Pickle path
+    with open('../datasets/production_model.pkl', 'rb') as pickle_in:
+        model = pickle.load(pickle_in)
 
-# Pickle path
-with open('../datasets/production_model.pkl', 'rb') as pickle_in:
-    model = pickle.load(pickle_in)
+    # Text input
+    your_text = st.text_input(
+    label='Please enter some text:',
+    value='bitcoin keeps mooning! do you think elon will pump dogecoin or should I keep buying ada?',
+    max_chars=500
+    )
 
-your_text = st.text_input(
-label='Please enter some text:',
-value='bitcoin keeps mooning! do you think elon will pump dogecoin or should I keep buying ada?',
-max_chars=1000
-)
-predicted_subreddit = model.predict([your_text])[0]
+    # Prediction
+    predicted_subreddit = model.predict([your_text])[0]
 
-st.subheader('Results:')
-st.write(f'You identify more closely with {predicted_subreddit.title()}')
+    # Labels
+    my_label = 'None'
+    my_asset = 'None'
+    if predicted_subreddit == 0:
+        my_label = 'r/SatoshiStreetBets'
+        my_asset = 'cryptocurrency'
+    elif predicted_subreddit == 1:
+        my_label = 'r/wallstreetbets'
+        my_asset = 'stocks'
+
+    # Results
+    st.write('''
+    
+    ''')
+    st.subheader('Results:')
+    st.write(f'This post resembles text you may find on **{my_label}**. You might identify more closely with **{my_asset}**.')
+    
+# Referenced: https://git.generalassemb.ly/DSIR-Lancelot/streamlit_lesson/blob/master/solution-code/app.py
