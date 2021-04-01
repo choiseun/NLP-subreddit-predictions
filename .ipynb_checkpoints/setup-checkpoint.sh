377f4294549e65a033db5d249a8cf56ebc7a090f
Seung Woo Choi <choiseun@alumni.usc.edu>
2021-04-01 18:27:43 -0400

    changes

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -1,13 +1,20 @@
-mkdir -p ~/.streamlit/
+# mkdir -p ~/.streamlit/
 
-echo “\
-[general]\n\
-email = \”choiseun@alumni.usc.edu\”\n\
-“ > ~/.streamlit/credentials.toml
+# echo “\
+# [general]\n\
+# email = \”choiseun@alumni.usc.edu\”\n\
+# “ > ~/.streamlit/credentials.toml
 
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+# echo “\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# “ > ~/.streamlit/config.toml
+
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
