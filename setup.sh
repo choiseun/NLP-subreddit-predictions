80493f1e041847f10f130357649e41a6eb02a04e
Seung Woo Choi <choiseun@alumni.usc.edu>
2021-04-03 21:27:52 -0400

    heroku references

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ headless = true
 port = $PORT
 enableCORS = false
 " > ~/.streamlit/config.toml
+
+# Referenced for Heroku: https://towardsdatascience.com/from-streamlit-to-heroku-62a655b7319
+# Referenced for Heroku: https://towardsdatascience.com/nlp-and-streamlit-on-heroku-5ebb56d6a57f
